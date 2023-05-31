9ec57c96af42ad71338ae0334b2ba0e9cfb01a1d
wakadesnehal <132550264+wakadesnehal@users.noreply.github.com>
2023-05-31 19:09:32 +0530

    Assignment 2

--- Program.cs
+++ Program.cs
@@ -1,107 +1,236 @@
-﻿using System;
-
-namespace Assignment3
+﻿namespace Assignment2
 {
-    internal class Program
+    internal class FavoriteMovie
     {
-        public abstract class clothing
+        static Dictionary<string, string> users = new Dictionary<string, string>();
+        static Dictionary<string, string> admins = new Dictionary<string, string>();
+        static Dictionary<string, string> movieCollection = new Dictionary<string, string>();
+        static Dictionary<string, string> favorites = new Dictionary<string, string>();
+
+        static bool isLoggedIn = false;
+        static bool isAdmin = false;
+        static bool isUser = false;
+
+        static void Main(string[] args)
         {
-            public abstract void sales();
 
-            public abstract void materials();
+            users.Add("user", "User@123");
+            admins.Add("Admin", "Admin@123");
+            Console.WriteLine("Welcome to the Fevorate Movie App!");
 
-            public abstract void commonwear();
-        }
+            string currentUser = "";
 
-        public class trendsetting : clothing
-        {
-            public override void sales()
+            while (true)
             {
-                Console.WriteLine("sales ");
+                Console.WriteLine("\nPlease select an option:");
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Exit");
 
-            }
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    continue;
+                }
 
-            public override void materials()
-            {
-                Console.WriteLine("materials");
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter your username: ");
+                        string username1 = Console.ReadLine();
+                        string username = username1.ToLower();
+                        Console.Write("Enter your password: ");
+                        string password = Console.ReadLine();
 
-            }
 
-            public override void commonwear()
-            {
-                Console.WriteLine("common wear ");
 
-            }
-        }
+                        if (admins.ContainsKey(username) && admins[username] == password)
+                        {
+                            isLoggedIn = true;
+                            isAdmin = true;
+                            currentUser = username;
+                            Console.WriteLine("\nAdmin login successful!");
+                        }
+                        else if (users.ContainsKey(username) && users[username] == password)
+                        {
+                            isLoggedIn = true;
+                            isUser = true;
+                            currentUser = username;
+                            Console.WriteLine("\nUser login successful!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nInvalid username or password. Please try again.");
+                        }
 
-        public class climatecontrol : clothing
-        {
-            public override void sales()
-            {
-                Console.WriteLine("sales by region for climatecontrol clothing:");
-            }
+                        break;
+                    case 2:
+                        Console.WriteLine("Exiting the Movie App. Goodbye!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
 
-            public override void materials()
-            {
-                Console.WriteLine("materials");
-
-            }
-
-            public override void commonwear()
-            {
-                Console.WriteLine("common wear");
-
-            }
-        }
-
-        public class sports : clothing
-        {
-            public override void sales()
-            {
-                Console.WriteLine("sales");
-
-            }
-
-            public override void materials()
-            {
-                Console.WriteLine("materials");
-
-            }
-
-            public override void commonwear()
-            {
-                Console.WriteLine("common wear");
-
-            }
-        }
-
-        class program
-        {
-            static void main(string[] args)
-            {
-                trendsetting trendsetting = new trendsetting();
-                trendsetting.sales();
-                trendsetting.materials();
-                trendsetting.commonwear();
-
-                climatecontrol climatecontrol = new climatecontrol();
-                climatecontrol.sales();
-                climatecontrol.materials();
-                climatecontrol.commonwear();
-
-                sports sports = new sports();
-                sports.sales();
-                sports.materials();
-                sports.commonwear();
-                Console.ReadLine();
+                if (isLoggedIn)
+                {
+                    if (isAdmin)
+                    {
+                        Admin();
+                    }
+                    else if (isUser)
+                    {
+                        User();
+                    }
+                }
             }
         }
 
+
+        static void Admin()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nAdmin Menu:");
+                Console.WriteLine("1. Show movie list");
+                Console.WriteLine("2. Add movie to collection");
+                Console.WriteLine("3. Remove movie from collection");
+                Console.WriteLine("4. Logout");
+
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        foreach (KeyValuePair<string, string> i in movieCollection)
+                        {
+                            Console.WriteLine(i.Key);
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Enter the name of the movie to add: ");
+                        string movie = Console.ReadLine();
+                        Console.Write("Enter the description of the movie: ");
+                        string description = Console.ReadLine();
+                        movieCollection.Add(movie, description);
+                        Console.WriteLine($"Movie '{movie}' added to the collection.");
+                        break;
+                    case 3:
+                        Console.WriteLine("Movies List");
+                        foreach (KeyValuePair<string, string> i in movieCollection)
+                        {
+                            Console.WriteLine(i.Key);
+                        }
+
+                        Console.Write("Enter the name of the movie to remove: ");
+                        string movieToRemove = Console.ReadLine();
+                        if (movieCollection.ContainsKey(movieToRemove))
+                        {
+                            movieCollection.Remove(movieToRemove);
+                            Console.WriteLine($"Movie '{movieToRemove}' removed from the collection.");
+                            RemoveMovieFromFavorites(movieToRemove);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Movie '{movieToRemove}' not found in the collection.");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Logging out from admin account.");
+                        isAdmin = false;
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static void User()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nUser Menu:");
+                Console.WriteLine("1. Show movie list");
+                Console.WriteLine("2. Add movie to favorites list");
+                Console.WriteLine("3. Remove movie from favorites list");
+                Console.WriteLine("4. Logout");
+
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Please select valid option.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 0:
+                        foreach (KeyValuePair<string, string> i in favorites)
+                        {
+                            Console.WriteLine(i.Key);
+                        }
+                        break;
+                    case 1:
+                        foreach (KeyValuePair<string, string> i in movieCollection)
+                        {
+                            Console.WriteLine(i.Key);
+                        }
+                        Console.Write("Enter the name of the movie to add to favorites list: ");
+                        string movie = Console.ReadLine();
+                        if (movieCollection.ContainsKey(movie))
+                        {
+                            favorites.Add(movie, movieCollection[movie]);
+                            Console.WriteLine($"Movie '{movie}' Added to favorites list.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Movie '{movie}' not found in the collection.");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Favorites List");
+                        foreach (KeyValuePair<string, string> i in favorites)
+                        {
+                            Console.WriteLine(i.Key);
+                        }
+
+                        Console.Write("Enter the name of the movie to remove from favorites list: ");
+                        string movieToRemove = Console.ReadLine();
+                        if (favorites.ContainsKey(movieToRemove))
+                        {
+                            favorites.Remove(movieToRemove);
+                            Console.WriteLine($"Movie '{movieToRemove}' removed from favorites list.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Movie '{movieToRemove}' not found in favorites list.");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Logged out from user account.");
+                        isUser = false;
+                        return;
+                    default:
+                        Console.WriteLine("Please select valid option.");
+                        break;
+                }
+            }
+        }
+
+        static void RemoveMovieFromFavorites(string movieToRemove)
+        {
+            if (favorites.ContainsKey(movieToRemove))
+            {
+                favorites.Remove(movieToRemove);
+                Console.WriteLine($"Movie '{movieToRemove}' removed from favoriteslist.");
+            }
+        }
 
     }
 }
-
-
-
-
-
